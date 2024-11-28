65795b432bf570a38e09ceb95095adeda9556185
Kirill <podgol04@gmail.com>
2024-11-28 22:42:58 +0300

    еще исправление

--- Assets/Script/Animation.cs
+++ Assets/Script/Animation.cs
@@ -1,71 +1,56 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
-    private Animator anim;
+    private Animator animator;
 
-    public string[] staticDirections = { "Static N", "Static NW", "Static W", "Static SW", "Static S", "Static SE", "Static E", "Static NE" };
-    public string[] runDirections = { "Run N", "Run NW", "Run W", "Run SW", "Run S", "Run SE", "Run E", "Run NE" };
+    private const int DirectionCount = 8; // Total number of directions
+    private const float AngleStep = 360f / DirectionCount; // Degrees per direction slice
+    private const float AngleOffset = AngleStep / 2; // Offset for angle calculation
 
-    int lastDirection;
+    private enum AnimationDirection
+    {
+        StaticN, StaticNW, StaticW, StaticSW,
+        StaticS, StaticSE, StaticE, StaticNE,
+        RunN, RunNW, RunW, RunSW,
+        RunS, RunSE, RunE, RunNE
+    }
 
     private void Awake()
     {
-        anim = GetComponent<Animator>();
-
-        float result1 = Vector2.SignedAngle(Vector2.up, Vector2.right);
-        Debug.Log("R1 " + result1);
-
-        float result2 = Vector2.SignedAngle(Vector2.up, Vector2.left);
-        Debug.Log("R2 " + result2);
-
-        float result3 = Vector2.SignedAngle(Vector2.up, Vector2.down);
-        Debug.Log("R3 " + result3);
+        animator = GetComponent<Animator>();
+      
     }
 
-    //MARKER each direction will match with one string element
-    //MARKER We used direction to determine their animation
-    public void SetDirection(Vector2 _direction)
+    public void SetDirection(Vector2 direction)
     {
-        string[] directionArray = null;
-
-        if (_direction.magnitude < 0.01)//MARKER Character is static. And his velocity is close to zero
+        if (direction.magnitude < 0.01f) // Character is static
         {
-            directionArray = staticDirections;
+            PlayAnimation(AnimationDirection.StaticN); // Default static animation
         }
         else
         {
-            directionArray = runDirections;
-
-            lastDirection = DirectionToIndex(_direction);//MARKER Get the index of the slcie from the direction vector
+            int directionIndex = GetDirectionIndex(direction);
+            PlayAnimation((AnimationDirection)(directionIndex + (int)AnimationDirection.RunN));
         }
-
-        anim.Play(directionArray[lastDirection]);
     }
 
-    //MARKER Converts a Vector2 direction to an index to a slcie around a circle
-    //CORE this goes in a counter-clock direction
-    private int DirectionToIndex(Vector2 _direction)
+    private void PlayAnimation(AnimationDirection direction)
     {
-        Vector2 norDir = _direction.normalized;//MARKER return this vector with a magnitude of 1 and get the normalized to an index
+        animator.Play(direction.ToString());
+    }
 
-        float step = 360 / 8;//MARKER 45 one circle and 8 slices//Calcuate how many degrees one slice is 
-        float offset = step / 2;//MARKER 22.5//OFFSET help us easy to calcuate and get the correct index of the string array
+    private int GetDirectionIndex(Vector2 direction)
+    {
+        Vector2 normalizedDirection = direction.normalized;
+        float angle = Vector2.SignedAngle(Vector2.up, normalizedDirection) + AngleOffset;
 
-        float angle = Vector2.SignedAngle(Vector2.up, norDir);//MARKER returns the signed angle in degrees between A and B
-
-        angle += offset;//Help us easy to calcuate and get the correct index of the string array
-
-        if (angle < 0)//avoid the negative number 
+        if (angle < 0)
         {
             angle += 360;
         }
 
-        float stepCount = angle / step;
-        return Mathf.FloorToInt(stepCount);
+        return Mathf.FloorToInt(angle / AngleStep) % DirectionCount; // Ensure index wraps around
     }
 }
-
