f056a89bc159ac660aed1de7772f9508276c898a
Kirill <podgol04@gmail.com>
2024-11-28 21:36:31 +0300

    полное добавление

--- Assets/Script/CharacterMove.cs
+++ Assets/Script/CharacterMove.cs
@@ -5,30 +5,31 @@ namespace Script
 {
     public class CharacterMove : MonoBehaviour
     {
-        private PlayerAnimation playerAnimation;
         private Rigidbody2D rb;
-
+        private float moveH, moveV;
         [SerializeField] private float moveSpeed = 1.0f;
 
         private void Awake()
         {
             rb = GetComponent<Rigidbody2D>();
-            playerAnimation = GetComponent<PlayerAnimation>();
         }
-
         private void FixedUpdate()
         {
-            Vector2 currentPos = rb.position;
-            float moveH = Input.GetAxis("Horizontal") * moveSpeed;
-            float moveV = Input.GetAxis("Vertical") * moveSpeed;
-            Vector2 inputVector = new Vector2(moveH, moveV);
+            moveH = Input.GetAxis("Horizontal") * moveSpeed;
+            moveV = Input.GetAxis("Vertical") * moveSpeed;
+            rb.linearVelocity = new Vector2(moveH, moveV);
 
-            inputVector = Vector2.ClampMagnitude(inputVector, 1);
-            Vector2 movement = inputVector * moveSpeed;
-            Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+            Vector2 direction = new Vector2(moveH, moveV);
 
-            rb.MovePosition(newPos);
-            playerAnimation.SetDirection(inputVector);
+            PlayerAnimation playerAnimation = FindObjectOfType<PlayerAnimation>();
+            if (playerAnimation != null)
+            {
+                playerAnimation.SetDirection(direction);
+            }
+            else
+            {
+                Debug.LogError("PlayerAnimation component not found in the scene.");
+            }
         }
     }
 }
